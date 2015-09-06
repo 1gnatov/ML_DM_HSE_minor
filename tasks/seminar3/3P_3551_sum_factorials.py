f26116b000b767ce48ff607a9e51addb5711dff3
Yury Kashnitsky <yury.kashnitsky@gmail.com>
2015-09-06 20:12:23 +0300

    add seminar 3 sum_factorials task

---
+++ tasks/seminar3/3P_3551_sum_factorials.py
@@ -0,0 +1,12 @@
+__author__ = 'yorko'
+
+n = int(input())
+
+def sum_factorials(n):
+    run_sum, total_sum = 1, 0
+    for i in xrange(n):
+        run_sum *= (i+1)
+        total_sum += run_sum
+    return total_sum
+
+print sum_factorials(n)
